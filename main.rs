c2c82cf3621ba2839d6c623425dec2e4f7c6194e
Christopher Krofchak <chris.krofchak@gmail.com>
2021-11-09 09:37:12 -0500

    macroized the macros... as would be expected for someone writing a macro...

--- main.rs
+++ main.rs
@@ -17,10 +17,40 @@ fn square(n: u32) -> u32 {
     n*n
 }
 
+macro_rules! mult_assert {
+    ($x:expr, $n:expr, $($f:expr),*) => {
+        $(
+            assert_eq!( $x, $f($n) );
+            println!("Assert passed! {}({}) is {}.", stringify!($f), $n, $x);
+        )*
+    };
+}
+
 fn main() {
-    let nested_1 = |n| square(double(succ(succ(n))));
+
+    // showing LTR and default pipe! behaves as LTR
+    let nested_ltr = |n| square(double(succ(succ(n))));
+    let ltr_piped_1 = ltr_pipe!(succ; succ; double; square);
     let piped_1 = pipe!(succ; succ; double; square);
 
-    assert_eq!(nested_1(1), 36);
-    assert_eq!(piped_1(1), 36);
+    mult_assert!( 36, 1, nested_ltr, ltr_piped_1, piped_1 );
+
+    // showing RTL 
+    let nested_rtl = |n| succ(succ(double(square(n))));
+    let rtl_piped_1 = rtl_pipe!(succ; succ; double; square);
+
+    mult_assert!( 20, 3, nested_rtl, rtl_piped_1 );
+
+    // just one final example where all 4 functions are different just so we guarantee
+    // what is going on is what's going on 
+    let triple = |n| k_n(3)(n);
+
+    let final_clos = |n| square(triple(double(succ(n))));
+    let rtl_final  = rtl_pipe!( square; triple; double; succ );
+    let ltr_final  = ltr_pipe!( succ; double; triple; square );
+    let pipe_fin   = pipe!( succ; double; triple; square );    
+
+    mult_assert!( 144, 1, final_clos, rtl_final, ltr_final, pipe_fin );
 }
+
+// ALL ASSERTIONS PASS!! 
--- pipe.rs
+++ pipe.rs
@@ -10,21 +10,21 @@ macro_rules! pipe {
     // base cases
     () => {|n| n}; // identity function
     ($x:expr) => {|n| $x(n)}; // return x function on its own
+    (LTR $x:expr) => {|n| $x(n)};
+    (RTL $x:expr) => {|n| $x(n)};
     // recursive reversal case
-    ($x:expr ; $($rest:expr);* ) => { comp!(pipe!($($rest);*) ; $x) }; // else |n| rest(x(n))
+    ($x:expr ; $($rest:expr);* ) => { comp!(pipe!($($rest);*) ; $x) };
+    (LTR $x:expr ; $($rest:expr);* ) => { comp!(pipe!(LTR $($rest);*) ; $x) };
+    (RTL $x:expr ; $($rest:expr);* ) => { comp!($x ; pipe!(RTL $($rest);*)) }; 
 }
 
 // same as pipe! but just for no ambiguity also good to have ltr and rtl...
 #[macro_export]
 macro_rules! ltr_pipe {
-    ($x:expr) => pipe!($x);
+    ($($x:tt)*) => {pipe!(LTR $($x)*)};
 }
 
 #[macro_export]
 macro_rules! rtl_pipe {
-    // base cases
-    () => {|n| n}; // identity function
-    ($x:expr) => {|n| $x(n)}; // return x function on its own
-    // recursive reversal case
-    ($x:expr ; $($rest:expr);* ) => { comp!($x ; pipe!($($rest);*)) }; // else |n| rest(x(n))
+    ($($x:tt)*) => {pipe!(RTL $($x)*)};
 }
